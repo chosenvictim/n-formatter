a02b3a63fac147c8c168d3ce373d4de411bcc60a
Shekhar Kumar <shekhar.kumar@bloomreach.com>
2018-07-01 17:34:53 +0000

    k-formatter function

---
+++ index.js
@@ -0,0 +1,20 @@
+export default function kFormatter(num, digits) {
+  var si = [
+    { value: 1e18, symbol: "E" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e3, symbol: "k" }
+  ];
+  var i;
+
+  for (i = 0; i < si.length; i++) {
+    if (num >= si[i].value) {
+      return (num / si[i].value).toFixed(digits).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") + si[i].symbol;
+    }
+  }
+
+  if (num) return num.toString();
+  else return num;
+}
